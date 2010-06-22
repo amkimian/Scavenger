5fb65dd7e77ecbdd94f34e0cc77fc94faade241c
Alan Moore <amkimian@mac.com>
2010-06-22 07:56:48 -0700

    Game playing

---
+++ Scavenger-iPad/Classes/GamePlaying/GameManager.h
@@ -0,0 +1,19 @@
+//
+//  GameManager.h
+//  Scavenger-iPad
+//
+//  Created by Alan Moore on 6/22/10.
+//  Copyright 2010 Mount Diablo Software (Alan Moore). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ * Playing a game
+ */
+
+@interface GameManager : NSObject {
+
+}
+
+@end
---
+++ Scavenger-iPad/Classes/GamePlaying/GameManager.m
@@ -0,0 +1,14 @@
+//
+//  GameManager.m
+//  Scavenger-iPad
+//
+//  Created by Alan Moore on 6/22/10.
+//  Copyright 2010 Mount Diablo Software (Alan Moore). All rights reserved.
+//
+
+#import "GameManager.h"
+
+
+@implementation GameManager
+
+@end
