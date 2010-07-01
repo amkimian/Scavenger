5423964179342cc89216100c8e12c7fba851045a
Alan Moore <amkimian@mac.com>
2010-07-01 16:54:24 -0700

    New LocationTools class

--- Scavenger-iPad/Classes/GameEditing/GameEditViewController.h
+++ Scavenger-iPad/Classes/GameEditing/GameEditViewController.h
@@ -15,21 +15,22 @@
 #import "MenuPopupController.h"
 #import "EditLocationController.h"
 #import "RouteListController.h"
+#import "LocationTools.h"
 
 @interface GameEditViewController : UIViewController<LocationOverlayViewDelegate,MapTypeChangedDelegate,ChooseListDidChooseDelegate,MenuPopupDelegate,EditLocationEndDelegate,
-RouteListDoneDelegate, MKReverseGeocoderDelegate> {
+RouteListDoneDelegate, LocationToolsDelegate> {
 	IBOutlet MKMapView *mapView;
 	GameObject *game;
 	LocationOverlayView *overlayView;
 	UIPopoverController *popOver;
 	CGRect savedRect;
-	MKReverseGeocoder *geocoder;
+	LocationTools *locationTools;
 }
 
 @property(nonatomic, retain) GameObject *game;
 @property(nonatomic, retain) LocationOverlayView *overlayView;
 @property(nonatomic, retain) UIPopoverController *popOver;
-@property(nonatomic, retain) MKReverseGeocoder *geocoder;
+@property(nonatomic, retain) LocationTools *locationTools;
 
 -(void) locationSelected: (LocationObject *) loc;
 -(void) locationSelectedAgain: (LocationObject *) loc atPoint: (CGPoint) p;
@@ -44,6 +45,5 @@ RouteListDoneDelegate, MKReverseGeocoderDelegate> {
 -(void) didSelectItem: (NSUInteger) item from:(MenuPopupController *) sender;
 
 -(void) editLocationDidFinishEditing:(EditLocationController *) controller;
--(void) alternateReverse: (CLLocationCoordinate2D) coordinate;
 
 @end
